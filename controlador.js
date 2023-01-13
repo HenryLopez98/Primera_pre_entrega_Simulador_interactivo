93201e73992981d5915c0c701bd094b871ed7e70
HenryLopez98 <117403112+HenryLopez98@users.noreply.github.com>
2023-01-12 20:25:00 -0500

    Add files via upload

---
+++ controlador.js
@@ -0,0 +1,14 @@
+
+
+function calcularedad(){
+    var edad = document.getElementById("edad").value;
+    
+
+if(edad==18){
+
+    alert("tu puedes entrar a nuestra pagina");
+}else{
+    alert("este contenido es prohibido para ti");
+
+}
+
